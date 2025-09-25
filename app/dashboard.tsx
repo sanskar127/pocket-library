0da7b3f2a1bcc102ed02371d8cadc560a3af1a00
Sanskar Pandey <sanskarpandey281@gmail.com>
2025-09-25 14:50:17 +0530

    Infinte scroll view replaced by load more button

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -4,7 +4,7 @@ import Video from '@/components/common/Video';
 import useFetchMedia from '@/hooks/useFetchMedia';
 import useLocalRouter from '@/hooks/useLocalRouter';
 import { DirectoryInterface, ImageInterface, VideoInterface } from '@/types/types';
-import { ScrollView, Text, View, SafeAreaView, ActivityIndicator, Button } from 'react-native';
+import { ScrollView, Text, View, ActivityIndicator, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 
 export default function Dashboard() {
   const { data, isLoading, hasMore, updateOffset } = useFetchMedia();
@@ -19,30 +19,37 @@ export default function Dashboard() {
     );
   }
 
+  // Scroll handler to detect when the user is near the bottom
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>): void => {
+    const contentHeight = event.nativeEvent.contentSize.height;
+    const contentOffsetY = event.nativeEvent.contentOffset.y;
+    const visibleHeight = event.nativeEvent.layoutMeasurement.height;
+
+    // Trigger fetching when near the bottom of the scroll
+    if (contentOffsetY + visibleHeight >= contentHeight - 20) {
+      if (hasMore) updateOffset()
+    }
+  };
+
   return (
-    <SafeAreaView className="flex-1 bg-background">
-      <ScrollView className='py-2'>
-        <View className="w-full p-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {data.map(item => {
-            if (item.type === "directory") {
-              return <Directory key={item.id} details={item as DirectoryInterface} />;
-            }
+    <ScrollView
+      className="w-full p-4 bg-background grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+      scrollEventThrottle={400} // Control the frequency of the onScroll event
+      onScroll={handleScroll}
+    >
+      {data.map(item => {
+        if (item.type === "directory") {
+          return <Directory key={item.id} details={item as DirectoryInterface} />;
+        }
 
-            if (item.type.startsWith("image/")) {
-              return <Image key={item.id} details={item as ImageInterface} />;
-            }
+        if (item.type.startsWith("image/")) {
+          return <Image key={item.id} details={item as ImageInterface} />;
+        }
 
-            if (item.type.startsWith("video/")) {
-              return <Video key={item.id} details={item as VideoInterface} />;
-            }
-          })}
-          {hasMore && <Button title='more' onPress={(e) => {
-            e.preventDefault()
-            updateOffset()
-          }
-          } />}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        if (item.type.startsWith("video/")) {
+          return <Video key={item.id} details={item as VideoInterface} />;
+        }
+      })}
+    </ScrollView>
   );
 }
